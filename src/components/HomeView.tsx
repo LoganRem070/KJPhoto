3b9ec1b05ae66e18e829f4ea7219ae464b15d553
LoganRem070 <153960090+LoganRem070@users.noreply.github.com>
2026-05-24 20:12:01 -0400

    Update HomeView.tsx

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -232,7 +232,7 @@ export default function HomeView({ setView }: HomeViewProps) {
           >
             <div className="absolute inset-0 z-0 overflow-hidden">
               <img
-                src= new URL('../assets/images/WeddingTitle.jpg', import.meta.url).href,
+                src={new URL('../assets/images/WeddingTitle.jpg', import.meta.url).href},
                 alt="Photography"
                 className="w-full h-full object-cover transition-transform duration-1000 group-hover:scale-105"
                 referrerPolicy="no-referrer"
